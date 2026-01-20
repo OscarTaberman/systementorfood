9f54ebdeb9ed59cd241f8684428596d64313208e
Oscar Taberman <oscartaberman@gmail.com>
2026-01-20 14:07:31 +0100

    Created Home.jsx and added in App.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <body>
+            <div className="hero">
+                <h1>Ristoranto</h1>
+            </div>
+            <div>
+                <h2>Welcome to Ristoranto!</h2>
+                <p>Your favorite place for delicious food.</p>
+            </div>
+        </body>
+    );
+};
