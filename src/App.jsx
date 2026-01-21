35f22696db2ff3bd9576e0967f01bfd34e627cd6
osmanjeylani <osmanjeylani00@gmail.com>
2026-01-21 14:08:05 +0100

    added cart icon and hover effects, nu ska den fkn funka

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import './components/Home.jsx'
 import Home from './components/Home.jsx'
+import Header from './components/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header/>
       <Home />
     </>
   )
