4a6672b321a3a6e2b7818475a8eb7e3ba332a292
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-01-22 14:21:21 +0100

    Small styling fixes. Connected navbar anchor to Contact-page.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,20 @@
 export default function Contact() {
     return (
         <>
-        <section className="contact-section">
+        <section className="contact-section" id="contact">
             <h2>Kontakta oss om du har några frågor!</h2>
             <h3>...vi kanske svarar.</h3>
             <form className="contact-form">
                 <ul>
-                <label htmlFor="email">E-post:</label>
-                <input type="email" id="email" name="email" required />
+                    <label htmlFor="email">E-post:</label>
+                    <input type="email" id="email" name="email" required />
                 </ul>
                 <ul>
-                <label htmlFor="message">Meddelande:</label>
-                <textarea id="message" name="message" required></textarea>
-                <button type="submit">Skicka</button>
+                    <label htmlFor="message">Meddelande:</label>
+                    <textarea id="message" name="message" required></textarea>
+                </ul>
+                <ul>
+                    <button type="submit">Skicka</button>
                 </ul>
             </form>
         </section>
