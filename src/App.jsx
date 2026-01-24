f57102804edfb730ee783a960dae37cee75c8e30
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-01-24 15:30:01 +0100

    Created Reference.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './components/Home.jsx'
 import Home from './components/Home.jsx'
 import Header from './components/Header.jsx'
 import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
       <Header/>
       <Home />
       <Contact />
+      <Footer />
     </>
   )
 }
