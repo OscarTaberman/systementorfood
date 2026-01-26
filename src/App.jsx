028ca6510aae664aef43481c3293e1c483235c34
Michael Fray <michaelfray1@gmail.com>
2026-01-26 09:50:30 +0100

    added social icons to footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import './components/Home.jsx'
-import Home from './components/Home.jsx'
-import Header from './components/Header.jsx'
+import { useState } from "react";
+import "./App.css";
+import "./components/Home.jsx";
+import Home from "./components/Home.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header/>
+      <Header />
       <Home />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,38 @@
-
-       import "./Footer.css";
+import "./Footer.css";
+import { FaLinkedin, FaTwitter, FaFacebook } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        
         <div className="footer-column">
           <h3>Systementor Education</h3>
           <p>Följ oss</p>
           <div className="socials">
-            <a href="#">LinkedIn</a>
-            <a href="#">Twitter</a>
-            <a href="#">Facebook</a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin size={24} />
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <FaTwitter size={24} />
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <FaFacebook size={24} />
+            </a>
           </div>
         </div>
 
         <div className="footer-column">
           <h4>Länkar</h4>
           <ul>
-            <li><a href="#">Om oss</a></li>
-            <li><a href="#">Jobba hos oss</a></li>
-            <li><a href="#">Referenser</a></li>
+            <li>
+              <a href="#">Om oss</a>
+            </li>
+            <li>
+              <a href="#">Jobba hos oss</a>
+            </li>
+            <li>
+              <a href="#">Referenser</a>
+            </li>
           </ul>
         </div>
 
@@ -30,7 +41,6 @@ function Footer() {
           <p>08-234-567</p>
           <p>info@systementor.se</p>
         </div>
-
       </div>
 
       <div className="footer-bottom">
